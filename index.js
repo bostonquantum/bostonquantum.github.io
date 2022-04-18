0f11d9c8612cd07a276182d4106d6b030a03b2c5
Phionx <shantanu.rajesh.jha@gmail.com>
2022-04-18 02:51:33 -0400

    updated site to v1 landing page

---
+++ index.js
@@ -0,0 +1,14 @@
+function toggleTheme() {
+    // Obtains an array of all <link>
+    // elements.
+    // Select your element using indexing.
+    var theme = document.getElementById('theme');
+
+    // Change the value of href attribute 
+    // to change the css sheet.
+    if (theme.getAttribute('href') == './node_modules/bootswatch/dist/minty/bootstrap.min.css') {
+        theme.setAttribute('href', './node_modules/bootswatch/dist/vapor/bootstrap.min.css');
+    } else {
+        theme.setAttribute('href', './node_modules/bootswatch/dist/minty/bootstrap.min.css');
+    }
+}
